7ae78fefa3035d6fc4d763361854f44dcbbbd052
Priscille Daoulas <priscilledaoulas@gmail.com>
2020-04-04 23:09:44 +0200

    CompositePersonnelDaoJDBC

--- pglp_5_2/src/main/java/priscille/pglp_5_2/CompositePersonnelDao.java
+++ pglp_5_2/src/main/java/priscille/pglp_5_2/CompositePersonnelDao.java
@@ -1,99 +1,135 @@
 package priscille.pglp_5_2;
 
-import java.sql.Array;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Map;
 
 public class CompositePersonnelDao
-extends Dao<CompositePersonnel> {
+implements Dao<CompositePersonnel>, Serializable {
+    /**
+     * Attribut de sérialisation.
+     */
+    private static final long serialVersionUID = 1L;
+    /**
+     * Liste des composites.
+     */
+    private ArrayList<CompositePersonnel> compositePersonnels;
     /**
      * Constructeur.
      */
-    public CompositePersonnelDao(Connection c) {
-        super(c);
+    public CompositePersonnelDao() {
+        this.compositePersonnels = new ArrayList<CompositePersonnel>();
     }
     /**
-     * Retourne le CompositePersonnel recherché.
+     * Retourne le composite recherché.
      * @param id L'identifiant du composite
-     * @return Le personnel trouvé
+     * @return Le composite personnel trouvé
      */
-    @Override
-    @SuppressWarnings({ "unchecked" })
-    public CompositePersonnel find(final int id) {
-        CompositePersonnel cp = null;
-        try {
-            PreparedStatement prepare = connect.prepareStatement("SELECT * FROM compositePersonnels WHERE id = ?");
-            prepare.setInt(1, id);
-            ResultSet result = prepare.executeQuery();
-            if (result.first()) {
-                cp = new CompositePersonnel();
-                ArrayList<InterfacePersonnel> liste =
-                        (ArrayList<InterfacePersonnel>) result.getArray("list");
-                for(InterfacePersonnel ip : liste) {
-                    cp.add(ip);
-                }
-                cp.setId(id);
+    public CompositePersonnel get(final int id) {
+        for (CompositePersonnel cp : compositePersonnels) {
+            if (cp.getId() == id) {
+                return cp;
             }
-        } catch(SQLException e) {
+        }
+        return null;
+    }
+    /**
+     * Retourne la liste.
+     * @return La liste du composite.
+     */
+    @SuppressWarnings("unchecked")
+    public ArrayList<CompositePersonnel> getAll() {
+        return (ArrayList<CompositePersonnel>)
+                compositePersonnels.clone();
+    }
+    /**
+     * Ajoute un composite a la liste.
+     * @param cp Le composite a ajouter
+     */
+    public void ajouter(final CompositePersonnel cp) {
+        compositePersonnels.add(cp);
+    }
+    /**
+     * Modifie un composite de la liste.
+     * @param cp Le composite a modifier
+     * @param params Le parametre a modifier
+     */
+    @SuppressWarnings("unchecked")
+    public void update(final CompositePersonnel cp,
+            final Map<String, Object> params) {
+        if (params.containsKey("list")) {
+            while (!cp.getList().isEmpty()) {
+                cp.remove(cp.getList().get(0));
+            }
+            ArrayList<InterfacePersonnel> nouvelle =
+                    (ArrayList<InterfacePersonnel>)
+                    params.get("list");
+            for (InterfacePersonnel ip : nouvelle) {
+                cp.add(ip);
+            }
+        }
+    }
+    /**
+     * Retire un composite de la liste.
+     * @param cp Le composite a retirer
+     */
+    public void retirer(final CompositePersonnel cp) {
+        compositePersonnels.remove(cp);
+    }
+    /**
+     * Fonction de sérialisation.
+     * @param path Adresse du fichier
+     */
+    public void serialization(final String path) {
+        ObjectOutputStream oos = null;
+        try {
+            final FileOutputStream fichierOut = new FileOutputStream(path);
+            oos = new ObjectOutputStream(fichierOut);
+            oos.writeObject(this);
+            oos.flush();
+            oos.close();
+        } catch (final java.io.IOException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                if (oos != null) {
+                    oos.flush();
+                    oos.close();
+                }
+            } catch (final java.io.IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+    /**
+     * Fonction de désérialisation.
+     * @param path Adresse du fichier
+     * @return Le personnelDao deserialisé
+     */
+    public static CompositePersonnelDao deSerialization(final String path) {
+        ObjectInputStream ois = null;
+        CompositePersonnelDao cp = null;
+        try {
+            final FileInputStream fichierIn = new FileInputStream(path);
+            ois = new ObjectInputStream(fichierIn);
+            cp = (CompositePersonnelDao) ois.readObject();
+        } catch (final java.io.IOException e) {
+            e.printStackTrace();
+        } catch (final ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (ois != null) {
+                    ois.close();
+                }
+            } catch (final java.io.IOException ex) {
+                ex.printStackTrace();
+            }
         }
         return cp;
-    }
-    /**
-     * Ajoute un CompositePersonnel.
-     * @param cpers Le composite a ajouter
-     */
-    @Override
-    public CompositePersonnel create(final CompositePersonnel cpers) {
-        try {
-            PreparedStatement prepare = connect.prepareStatement(
-                    "INSERT INTO compositePersonnels (id,list)"
-                    + "VALUES (?,?)");
-            prepare.setInt(1, cpers.getId());
-            prepare.setArray(2, (Array) cpers.getList());
-            int result = prepare.executeUpdate();
-            assert result == 1;
-        } catch(SQLException e) {
-            e.printStackTrace();
-        }
-        return cpers;
-    }
-    /**
-     * Modifie un CompositePersonnel.
-     * @param cpers Le composite a modifier
-     */
-    @Override
-    @SuppressWarnings({ })
-    public CompositePersonnel update(final CompositePersonnel cpers) {
-        try {
-            PreparedStatement prepare = connect.prepareStatement(
-                    "UPDATE compositePersonnels SET list = ? WHERE id = ?");
-            prepare.setArray(1, (Array) cpers.getList());
-            prepare.setInt(2, cpers.getId());
-            int result = prepare.executeUpdate();
-            assert result == 1;
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return cpers;
-    }
-    /**
-     * Retire un CompositePersonnel.
-     * @param cpers Le composite a retirer
-     */
-    @Override
-    public void delete(final CompositePersonnel cpers) {
-        try {
-            PreparedStatement prepare = connect.prepareStatement(
-                    "DELETE FROM compositePersonnels WHERE id = ?");
-            prepare.setInt(1,  cpers.getId());
-            int result = prepare.executeUpdate();
-            assert result == 1;
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
     }
 }
